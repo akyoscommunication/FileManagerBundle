f352fc4d2a50af739f35e311a833c80ad60940e7
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2020-03-02 16:53:56 +0100

    Add filemanager front js

--- assets/scripts/front/modules/lazyLoad.js
+++ assets/scripts/front/modules/lazyLoad.js
@@ -14,7 +14,10 @@ class LazyLoad {
 			if (image.classList.contains('not-loaded')) {
 				const rect = image.getBoundingClientRect();
 				const html = document.documentElement;
-				if (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || html.clientHeight) && rect.right <= (window.innerWidth || html.clientWidth)) {
+				if (
+					(rect.top >= 0 && rect.top < (window.innerHeight || html.clientHeight))
+					|| (rect.bottom > 0 && rect.bottom <= (window.innerHeight || html.clientHeight)) )
+				{
 					$(image).attr('src', $(image).data('src'));
 					image.classList.remove('not-loaded')
 				}
