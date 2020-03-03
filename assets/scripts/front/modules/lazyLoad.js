0866ae545bfed40cf603d935134daf40e2c3b1bc
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2020-03-03 16:50:31 +0100

    Fix lazyload

--- assets/scripts/front/modules/lazyLoad.js
+++ assets/scripts/front/modules/lazyLoad.js
@@ -15,8 +15,11 @@ class LazyLoad {
 				const rect = image.getBoundingClientRect();
 				const html = document.documentElement;
 				if (
-					(rect.top >= 0 && rect.top < (window.innerHeight || html.clientHeight))
-					|| (rect.bottom > 0 && rect.bottom <= (window.innerHeight || html.clientHeight)) )
+					((rect.top >= 0 && rect.top < (window.innerHeight || html.clientHeight))
+					|| (rect.bottom > 0 && rect.bottom <= (window.innerHeight || html.clientHeight)))
+					&&
+					((rect.left >= 0 && rect.left < (window.innerWidth || html.clientWidth))
+					|| (rect.right > 0 && rect.right <= (window.innerWidth || html.clientWidth))) )
 				{
 					$(image).attr('src', $(image).data('src'));
 					image.classList.remove('not-loaded')
