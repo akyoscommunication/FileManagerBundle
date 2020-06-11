76987bacaf40d4305ea6353cd4c28328019d6a5c
Thomas Sébert <tom@Mac-mini-de-Thomas-2.home>
2020-06-11 17:57:00 +0200

    Add security controller for projects that need to access file manager from different user providers

---
+++ Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Akyos\FileManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * @Route("/file-manager", name="file_manager_")
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@AkyosCore/security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+}
