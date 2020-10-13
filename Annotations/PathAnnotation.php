30408a1c37f283d5febf63a90722a577552dbbc1
Lilian Carneau <liliancarneau@mini-de-lilian.home>
2020-10-13 09:13:11 +0200

    feat: PathImgAnnotation

---
+++ Annotations/PathAnnotation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Akyos\FileManagerBundle\Annotations;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation
+ * @Target("PROPERTY")
+ */
+class PathAnnotation
+{
+    private $field;
+
+    public function __construct(array $values)
+    {
+        $this->field = $values['field'];
+    }
+
+    public function getField()
+    {
+        return $this->field;
+    }
+
+}
---
+++ DoctrineListener/PathAnnotationListener.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Akyos\FileManagerBundle\DoctrineListener;
+
+use Akyos\FileManagerBundle\Annotations\PathAnnotation;
+use Akyos\FileManagerBundle\Entity\File;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+class PathAnnotationListener
+{
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        // Using reflection so we can inspect properties and their annotations later
+        $reflectionObject = new \ReflectionObject($entity);
+
+        AnnotationRegistry::registerUniqueLoader('class_exists');
+
+        $reader = new AnnotationReader;
+
+        // Iterate over properties to find those targeted by a @PathAnnotation
+        foreach ($reflectionObject->getProperties() as $entityTargetProperty) {
+
+            // This will be null if the property does not have an @PathAnnotation on it
+            $pathAnnotation = $reader->getPropertyAnnotation($entityTargetProperty, PathAnnotation::class);
+
+            if (null !== $pathAnnotation) {
+                $sourcePropertyName = $pathAnnotation->getField();
+
+                $fileIdGetterName = 'get' . ucfirst($sourcePropertyName);
+                $targetPropertySetterName = 'set' . $entityTargetProperty->getName();
+
+                $fileId = $entity->{$fileIdGetterName}();
+
+                $em = $args->getEntityManager();
+                $file = $em->getRepository(File::class)->findOneBy(['id' => $fileId]);
+
+                if ($file) {
+                    $entity->{$targetPropertySetterName}($file->getFile());
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        // Using reflection so we can inspect properties and their annotations later
+        $reflectionObject = new \ReflectionObject($args->getObject());
+
+        AnnotationRegistry::registerUniqueLoader('class_exists');
+
+        $reader = new AnnotationReader;
+
+        // Iterate over properties to find those targeted by a @PathAnnotation
+        foreach ($reflectionObject->getProperties() as $entityTargetProperty) {
+
+            // This will be null if the property does not have an @PathAnnotation on it
+            $pathAnnotation = $reader->getPropertyAnnotation($entityTargetProperty, PathAnnotation::class);
+
+            if (null !== $pathAnnotation) {
+
+                $em = $args->getEntityManager();
+                $unitOfWork = $em->getUnitOfWork();
+                $changeSet = $unitOfWork->getEntityChangeSet($entity);
+
+                $sourcePropertyName = $pathAnnotation->getField();
+                $isPropertyUpdated = array_key_exists($sourcePropertyName, $changeSet);
+
+                if ($isPropertyUpdated) {
+                    $getterName = 'get' . ucfirst($pathAnnotation->getField());
+                    $setterName = 'set' . $entityTargetProperty->getName();
+
+                    $fileId = $entity->{$getterName}();
+
+                    $file = $em->getRepository(File::class)->findOneBy(['id' => $fileId]);
+
+                    if ($file) {
+                        $entity->{$setterName}($file->getFile());
+
+                        $em->flush();
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
