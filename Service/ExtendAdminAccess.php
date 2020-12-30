504bffc09a36f06d8529e9e63673222014be5be6
Rudy Masson <rmasson.pro@gmail.com>
2020-12-30 15:08:09 +0100

    service/extendAdminAccess

---
+++ Service/ExtendAdminAccess.php
@@ -0,0 +1,44 @@
+<?php
+namespace Akyos\FileManagerBundle\Service;
+
+use Akyos\CoreBundle\Entity\AdminAccess;
+use Akyos\CoreBundle\Repository\AdminAccessRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ExtendAdminAccess
+{
+    private AdminAccessRepository $adminAccessRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(AdminAccessRepository $adminAccessRepository, EntityManagerInterface $entityManager)
+    {
+        $this->adminAccessRepository = $adminAccessRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    public function setDefaults()
+    {
+        if (!$this->adminAccessRepository->findOneByName("Gestion des fichiers"))
+        {
+            $adminAccess = new AdminAccess();
+            $adminAccess
+                ->setName('Gestion des fichiers')
+                ->setRoles([])
+                ->setIsLocked(true)
+            ;
+            $this->entityManager->persist($adminAccess);
+            $this->entityManager->flush();
+        }
+        if (!$this->adminAccessRepository->findOneByName("Options du Gestionnaire de fichier"))
+        {
+            $adminAccess = new AdminAccess();
+            $adminAccess
+                ->setName('Options du Gestionnaire de fichier')
+                ->setRoles([])
+                ->setIsLocked(true)
+            ;
+            $this->entityManager->persist($adminAccess);
+            $this->entityManager->flush();
+        }
+    }
+}
--- Service/ExtendSidebar.php
+++ Service/ExtendSidebar.php
@@ -4,22 +4,27 @@ namespace Akyos\FileManagerBundle\Service;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Security;
 
 class ExtendSidebar
 {
     private $router;
+    private $security;
 
-    public function __construct(UrlGeneratorInterface $router)
+    public function __construct(UrlGeneratorInterface $router, Security $security)
     {
         $this->router = $router;
+        $this->security = $security;
     }
 
     public function getTemplate($route)
     {
-        $template = '
-        <li class="'.(strpos($route,"file_show") !== false ? "active" : "").'">
-            <a href="'.$this->router->generate('file_show').'">Gestion des fichiers</a>
-        </li>';
+        $template ='';
+        if($this->security->isGranted('gestion-des-fichiers')){
+            $template = '<li class="'.(strpos($route,"file_show") !== false ? "active" : "").'">
+                            <a href="'.$this->router->generate('file_show').'">Gestion des fichiers</a>
+                        </li>';
+        }
         return new Response($template);
     }
 }
